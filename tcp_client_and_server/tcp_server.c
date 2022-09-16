efee28a281da23939e81beb240d408643621244d
Odey John Ebinyi <odeyjhn@gmail.com>
2022-09-16 11:20:07 +0100

    Update tcp_server.c

--- tcp_client_and_server/tcp_server.c
+++ tcp_client_and_server/tcp_server.c
@@ -11,37 +11,40 @@ int main()
 {
     printf("Configuring local address.on port 8080..\n");
     
-    struct addrinfo hints;
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
+    struct addrinfo info;
+    memset(&info, 0, sizeof(info));
+    info.ai_family = AF_INET;
+    info.ai_socktype = SOCK_STREAM;
+    info.ai_flags = AI_PASSIVE;
 
     struct addrinfo *bind_address;
-    getaddrinfo(0, "8080", &hints, &bind_address);
+    getaddrinfo(0, "8080", &info, &bind_address);
 
 
     printf("Creating socket...\n");
     SOCKET socket_listen;
     socket_listen = socket(bind_address->ai_family,
             bind_address->ai_socktype, bind_address->ai_protocol);
-    if (!IS_VALID_SOCKET(socket_listen)) {
-        fprintf(stderr, "socket() failed. (%d)\n", GET_SOCKET_ERRNO());
+    if (!IS_VALID_SOCKET(socket_listen)) 
+    {
+        fprintf(stderr, "ERROR: socket() failed - (%d)\n", GET_SOCKET_ERRNO());
         return 1;
     }
 
 
     printf("Binding socket to local address...\n");
-    if (bind(socket_listen,
-                bind_address->ai_addr, bind_address->ai_addrlen)) {
-        fprintf(stderr, "bind() failed. (%d)\n", GET_SOCKET_ERRNO());
+    if (bind(socket_listen, bind_address->ai_addr, bind_address->ai_addrlen)) 
+    {
+        fprintf(stderr, "ERROR: bind() failed - (%d)\n", GET_SOCKET_ERRNO());
         return 1;
     }
+
     freeaddrinfo(bind_address);
 
 
     printf("Listening...\n");
-    if (listen(socket_listen, 10) < 0) {
+    if (listen(socket_listen, 10) < 0)
+    {
         fprintf(stderr, "listen() failed. (%d)\n", GET_SOCKET_ERRNO());
         return 1;
     }
@@ -49,32 +52,32 @@ int main()
     printf("Waiting for connections...\n");
 
 
-     struct sockaddr_storage client_address;
-     socklen_t client_len = sizeof(client_address);
-     SOCKET socket_client = accept(socket_listen, (struct sockaddr*) &client_address, &client_len);
+    struct sockaddr_storage client_address;
+    socklen_t client_len = sizeof(client_address);
+    SOCKET socket_client = accept(socket_listen, (struct sockaddr*) &client_address, &client_len);
      
-     if (!IS_VALID_SOCKET(socket_client)) 
-     {
-        fprintf(stderr, "accept() failed. (%d)\n", GET_SOCKET_ERRNO());
-                return 1;
-     }
+    if (!IS_VALID_SOCKET(socket_client)) 
+    {
+       fprintf(stderr, "accept() failed. (%d)\n", GET_SOCKET_ERRNO());
+               return 1;
+    }
      
-     printf("Reading request...\n");
-     char req[4096];
-     int bytes_recv = recv(socket_client, req, 4096, 0);
-     printf("Received bytes: %d.\n", bytes_recv);
-     printf("Message: %.*s", bytes_recv, req);
+    printf("Reading request...\n");
+    char req[4096];
+    int bytes_recv = recv(socket_client, req, 4096, 0);
+    printf("Received bytes: %d.\n", bytes_recv);
+    printf("Message: %.*s", bytes_recv, req);
      
-     printf("Sending response...\n");
-     char resp_buf[4096] = "Hello, i got your message!.\n";
-     int bytes_sent = send(socket_client, resp_buf, strlen(resp_buf), 0);
-     printf("Sent %d of %d bytes.\n", bytes_sent, (int)strlen(resp_buf));
+    printf("Sending response...\n");
+    char resp_buf[4096] = "Hello, i got your message!.\n";
+    int bytes_sent = send(socket_client, resp_buf, strlen(resp_buf), 0);
+    printf("Sent %d of %d bytes.\n", bytes_sent, (int)strlen(resp_buf));
      
 	
-     printf("Closing listening socket...\n");
-     CLOSE_SOCKET(socket_listen);
+    printf("Closing listening socket...\n");
+    CLOSE_SOCKET(socket_listen);
 
-     printf("Connection Terminated.\n");
+    printf("Connection Terminated.\n");
 
-return 0;
+    return 0;
 }
